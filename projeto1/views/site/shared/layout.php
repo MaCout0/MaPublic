e0ea8e0f51c1b25e2d02accc300dad1b1a924b95
MaCout0 <jkmbint@gmail.com>
2020-09-01 13:46:28 -0300

    MaPublic

---
+++ projeto1/views/site/shared/layout.php
@@ -0,0 +1,20 @@
+<?php 
+	$url = $this->helpers['URLHelper']->getURL();
+	$location  = $this->helpers['URLHelper']->getLocation();
+ ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="utf-8">
+		<meta name="keywords" content="HTML, CSS, JS, PHP">
+		<meta name="description" content=" Descrição do site">
+		<meta name="author" content="Code Universe">
+		<meta name="viewport" content="width=device-width, initial-scale1.0">
+		<meta name="robots" content="index, follow">
+		<title>Projeto</title>
+	</head>
+	<body>
+
+	</body>
+</html>

cc8fb23893059f40897a8215c6ccd20a51ff0ca5
MaCout0 <jkmbint@gmail.com>
2020-09-04 14:13:40 -0300

    MaPublic

---
+++ projeto1/views/site/shared/header.php
@@ -0,0 +1,21 @@
+<!-- Cabeçalho -->
+<div class="container">
+	
+	<!-- logo -->
+	<div class="float-left pt-3">
+		<a href="#" class="text-light">Logo</a>
+	</div>
+
+	<!-- hamburger -->
+	<div class="hamburger d-none float-right">
+		<img src="<?php echo $url; ?>/assets/img/201.png">
+	</div>
+
+	<!-- link -->
+	<div class="links">
+		<a href="#" class="text-light d-block float-right">Home</a>
+		<a href="#" class="text-light d-block float-right">Quem Somos</a>
+		<a href="#" class="text-light d-block float-right">Contato</a>
+	</div>
+
+</div>
